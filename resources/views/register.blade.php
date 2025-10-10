d5b828dfbed8e27fd13e62642d08d70f2b6dfac0
Restu Ardiansyah <ardiansyahrestu713@gmail.com>
2025-10-10 17:37:42 +0700

    update register

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -3,14 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    {{-- Tailwind + font.css --}}
+    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/css/font.css'])
+
+    {{-- Font Awesome (pindahkan ke bawah) --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Register Page</title>
-    @vite('resources/css/app.css')
 </head>
 <body class="bg-[#2E8BFD] font-[poppins] flex items-center justify-center min-h-screen px-4">
    <div class="flex flex-col md:flex-row items-center justify-between gap-10 md:gap-20 w=full max-w-6xl">
     <!-- Logo -->
     <div class="hidden md:flex flex-col md:flex-row items-center justify-center md:justify-start gap-4 text-center md:text-left">
-     <img src="" alt="Logo" class="bg-transparent object-contain w-48 h-48">    
+     <img src="{{ asset('img/kuning-nobg.png') }}" alt="Logo" class="bg-transparent object-contain w-48 h-48">    
      <p class="text-4xl md:text-5xl text-[#FDBA38] font-semibold">MahasisMart</p>
     </div>
 
@@ -44,7 +48,7 @@
              type="button"
              class="bg-white cursor-pointer text-zinc-800 font-medium w-full p-3 mt-6 rounded-md border border-zinc-400
             hover:bg-zinc-100 transition flex items-center justify-center gap-3">
-             <i class="fa-brands fa-google text-[#FDBA38]"></i>   
+             <i class="fa-brands fa-google"></i>   
              Google
              </button>
 
--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-<<<<<<< HEAD
 Route::get('/register', function () {
     return view('register');
 });
-=======
+
 Route::get('/login', function () {
     return view('login');
 });
->>>>>>> 497659db255ed0d4fc332b221db3cebe8778d8fb
