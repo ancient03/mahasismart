299c1f4e5e8892c377a487bb52b8d5f5f3322f74
ancientxd <rizqieadriananto@gmail.com>
2025-10-24 23:35:28 +0700

    memperbaiki sidebar

--- resources/views/components/sidebar/sidebar-profile.blade.php
+++ resources/views/components/sidebar/sidebar-profile.blade.php
@@ -3,9 +3,12 @@
                     
                     <div class="flex items-center space-x-4 mb-6">
                         <div class="w-16 h-16 bg-gray-200 rounded-full flex items-center justify-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 text-gray-500" viewBox="0 0 20 20" fill="currentColor">
-                                <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd" />
-                            </svg>
+                            @if(Auth::user()->foto_profil)
+                                <img src="{{ asset('img/fotoprofile/' . Auth::user()->foto_profil) }}" alt="Foto Profil" class="w-16 h-16 rounded-full object-cover">
+                            @else
+                                <i class="bi bi-person-fill text-4xl text-gray-400"></i>
+                            @endif
+
                         </div>
                         <div>
                             <span class="font-bold text-lg">{{ Auth::user()->username }}</span>
@@ -24,16 +27,15 @@
         <span>Profil</span>
     </a>
 
-    <!-- Link Alamat (Dinamis) -->
-    <a href="alamat" 
-       @class([
-           'flex items-center space-x-3 py-2 px-3 rounded-md', // Kelas dasar
-           'bg-green-100 text-green-700 font-semibold' => Route::is('alamat'), // Kelas aktif
-           'text-gray-600 hover:bg-gray-100 hover:text-gray-900' => !Route::is('alamat') // Kelas inaktif
-       ])>
-        <span>Alamat</span>
-    </a>
-
+<!-- Link Alamat (Dinamis Diperbaiki) -->
+<a href="{{ route('alamat.index') }}" {{-- 1. Perbaiki href --}}
+   @class([
+       'flex items-center space-x-3 py-2 px-3 rounded-md', // Kelas dasar
+       'bg-green-100 text-green-700 font-semibold' => Route::is('alamat.*'), // 2. Gunakan wildcard '*'
+       'text-gray-600 hover:bg-gray-100 hover:text-gray-900' => !Route::is('alamat.*') // 2. Gunakan wildcard '*'
+   ])>
+    <span>Alamat</span>
+</a>
     <!-- Link Pesanan Saya (Ganti '#' dengan route Anda nanti) -->
     <a href="pesanan" 
        @class([
