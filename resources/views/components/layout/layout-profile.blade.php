bdcd252bd91efeaada1a8c4d50f368e8d80c4f05
ancient03 <rizqieadriananto@gmail.com>
2025-12-24 03:41:44 +0700

    Menambahkan halaman detail toko dan memperbaiki bug

--- resources/views/components/layout/layout-profile.blade.php
+++ resources/views/components/layout/layout-profile.blade.php
@@ -29,5 +29,6 @@
     <x-nav-mobile/>
     <x-modal-success /> {{-- Panggil komponen modal --}}
     <x-modal-error />
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </body>
 </html>
