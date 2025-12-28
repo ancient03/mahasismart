b6aae4d3509b9dab201055f91582b6959bd02d6d
ancient03 <rizqieadriananto@gmail.com>
2025-12-28 23:45:37 +0700

    push oi pushh

--- config/midtrans.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'merchant_id' => env('MIDTRANS_MERCHANT_ID', null),
-    'client_key' => env('MIDTRANS_CLIENT_KEY', null),
-    'server_key' => env('MIDTRANS_SERVER_KEY', null),
-    'is_production' => env('MIDTRANS_IS_PRODUCTION', false),
-    'snap_url' => env('MIDTRANS_IS_PRODUCTION', false) ? 'https://app.midtrans.com/snap/snap.js' : 'https://app.sandbox.midtrans.com/snap/snap.js',
-];
